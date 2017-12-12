d2ffd418fef0ce16d3d191594f72795558ae9b2a
yska <lagache.dimitri59@gmail.com>
2017-12-13 00:19:14 +0100

    amelioration Js

--- js/app.js
+++ js/app.js
@@ -4,13 +4,37 @@ function ajouter(balise, texte){
     balise.appendChild(contenu);
     return balise;
 }
+
+// uderline permanant sous liens actif
+var chemin = document.location.href;
+var url = chemin.split("/").pop(-1);
+
+listeDesLiens = document.getElementById("nav-bar").getElementsByTagName("a");
+for (var i=0; i<listeDesLiens.length; i++) {
+    // on compare le href avec le chemin de la page en cours
+    if(listeDesLiens[i].getAttribute("href") == url){
+        // on modifie le style du lien actif
+        listeDesLiens[i].style.textDecoration = "underline #f9a918";
+
+        // on sort de la boucle
+        break;
+    }
+
+}
+
 var btn = document.getElementById('commencer');
 var login = document.getElementById('home');
 var logged = document.getElementById('start');
-btn.onclick= function () {{
+btn.onclick= function () {
         home.style.display = 'none';
         start.style.display ='block';
         logged.appendChild(Welcome);
-    }
-};
+    };
+
+
+
+
+
+
+
 
